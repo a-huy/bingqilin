bbe560cd71cf19fd1839fbd6b236ae0d2b044042
Andy Nguyen <anguyenhuy@gmail.com>
2023-11-03 16:35:03 -0700

    add test for setting default field

--- tests/unit/test_contexts.py
+++ tests/unit/test_contexts.py
@@ -13,3 +13,15 @@ class TestLifespanContexts(BaseTestCase):
 
         ctx = Context(test_settings_manager).configure()
         self.assertIsInstance(ctx.test_field, TestObject)
+
+    def test_default_field(self, test_settings_manager):
+        class TestObject:
+            def __init__(self, config):
+                self.config = config
+
+        class Context(LifespanContext):
+            test_field: TestObject = ContextField("test", is_default=True)
+
+        ctx = Context(test_settings_manager).configure()
+        self.assertEqual(ctx.get_default("test"), ctx.test_field)
+        self.assertEqual(ctx.get_default(), ctx.test_field)
